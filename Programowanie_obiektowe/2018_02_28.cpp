62bbdcab5c13c47f718aef06c250dc93d4516b76
TeachMeHoW <szymoncichy123@gmail.com>
2018-02-28 16:42:04 +0100

    main function with called Board constructor

--- Programowanie_obiektowe/2018_02_28.cpp
+++ Programowanie_obiektowe/2018_02_28.cpp
@@ -1,8 +1,9 @@
-#include <boost/archive/text_iarchive.hpp>
-#include <boost/archive/text_oarchive.hpp>
-
+#include "Board.h"
+#include "Field.h"
 int main()
 {
-
+	Board brd =Board(20, 30);
+	Board(20);
+	Board();
 	return 0;
 }
