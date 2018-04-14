442df9b6ccc732a79fe9c0b650d98ee801e1e6be
TeachMeHoW <szymoncichy123@gmail.com>
2018-04-14 14:21:51 +0200

    grid relative positions

--- Programowanie_obiektowe/Intro.cpp
+++ Programowanie_obiektowe/Intro.cpp
@@ -43,62 +43,61 @@ void Intro::draw(sf::RenderWindow & window, sf::Font font)
 	sf::Text btn_text;
 	btn_text.setFillColor(black);
 	btn_text.setFont(font);
-	int char_size = 20;
-	btn_text.setCharacterSize(char_size);
+	btn_text.setCharacterSize(title_char_size);
 	// Row 1 title
 	btn_text.setString("Board size");
-	btn_text.setPosition(grid_pos + sf::Vector2f(grid_spacing.x * (1 + 0) + btn_size.x * 0, grid_spacing.y * (0 + 0) - char_size));
+	btn_text.setPosition(grid_pos);
 	window.draw(btn_text);
 	// grid 0,0
 	btn_text.setString("8x8");
-	btn_text.setPosition(grid_pos + sf::Vector2f(grid_spacing.x * (1 + 0) + btn_size.x * 0, grid_spacing.y * (1 + 0) + btn_size.y * 0));
-	btn.setPosition(grid_pos + sf::Vector2f(grid_spacing.x * (1 + 0) + btn_size.x * 0, grid_spacing.y * (1 + 0) + btn_size.y * 0));
+	btn_text.setPosition(row_0_btn_0_pos);
+	btn.setPosition(row_0_btn_0_pos);
 	btn.setFillColor(btn_inactive);
-	if (row_1_active_button == 0) btn.setFillColor(btn_active);
+	if (row_0_active_button == 0) btn.setFillColor(btn_active);
 	window.draw(btn);
 	window.draw(btn_text);
-	// grid 0,1
+	// grid 1,0
 	btn_text.setString("12x12");
-	btn_text.setPosition(grid_pos + sf::Vector2f(grid_spacing.x * (1 + 1) + btn_size.x * 1, grid_spacing.y * (1 + 0) + btn_size.y * 0));
-	btn.setPosition(grid_pos + sf::Vector2f(grid_spacing.x * (1 + 1) + btn_size.x * 1, grid_spacing.y * (1 + 0) + btn_size.y * 0));
+	btn_text.setPosition(row_0_btn_1_pos);
+	btn.setPosition(row_0_btn_1_pos);
 	btn.setFillColor(btn_inactive);
-	if (row_1_active_button == 1) btn.setFillColor(btn_active);
+	if (row_0_active_button == 1) btn.setFillColor(btn_active);
 	window.draw(btn);
 	window.draw(btn_text);
-	// grid 0,2
+	// grid 2,0
 	btn_text.setString("16x16");
-	btn_text.setPosition(grid_pos + sf::Vector2f(grid_spacing.x * (1 + 2) + btn_size.x * 2, grid_spacing.y * (1 + 0) + btn_size.y * 0));
-	btn.setPosition(grid_pos + sf::Vector2f(grid_spacing.x * (1 + 2) + btn_size.x * 2, grid_spacing.y * (1 + 0) + btn_size.y * 0));
+	btn_text.setPosition(row_0_btn_2_pos);
+	btn.setPosition(row_0_btn_2_pos);
 	btn.setFillColor(btn_inactive);
-	if (row_1_active_button == 2) btn.setFillColor(btn_active);
+	if (row_0_active_button == 2) btn.setFillColor(btn_active);
 	window.draw(btn);
 	window.draw(btn_text);
 	// Second row title
 	btn_text.setString("Difficulty");
-	btn_text.setPosition(grid_pos + sf::Vector2f(grid_spacing.x * (1 + 0) + btn_size.x * 0, grid_spacing.y * (1 + 1) + btn_size.y * 1));
+	btn_text.setPosition(title_2_pos);
 	window.draw(btn_text);
 	// grid 1,0
 	btn_text.setString("Easy");
-	btn_text.setPosition(grid_pos + sf::Vector2f(grid_spacing.x * (1 + 0) + btn_size.x * 0, grid_spacing.y * (2 + 1) + btn_size.y * 1 + char_size));
-	btn.setPosition(grid_pos + sf::Vector2f(grid_spacing.x * (1 + 0) + btn_size.x * 0, grid_spacing.y * (2 + 1) + btn_size.y * 1 + char_size));
+	btn_text.setPosition(row_1_btn_0_pos);
+	btn.setPosition(row_1_btn_0_pos);
 	btn.setFillColor(btn_inactive);
-	if (row_2_active_button == 0) btn.setFillColor(btn_active);
+	if (row_1_active_button == 0) btn.setFillColor(btn_active);
 	window.draw(btn);
 	window.draw(btn_text);
 	// grid 1,1
 	btn_text.setString("Medium");
-	btn_text.setPosition(grid_pos + sf::Vector2f(grid_spacing.x * (1 + 1) + btn_size.x * 1, grid_spacing.y * (2 + 1) + btn_size.y * 1 + char_size));
-	btn.setPosition(grid_pos + sf::Vector2f(grid_spacing.x * (1 + 1) + btn_size.x * 1, grid_spacing.y * (2 + 1) + btn_size.y * 1 + char_size));
+	btn_text.setPosition(row_1_btn_1_pos);
+	btn.setPosition(row_1_btn_1_pos);
 	btn.setFillColor(btn_inactive);
-	if (row_2_active_button == 1) btn.setFillColor(btn_active);
+	if (row_1_active_button == 1) btn.setFillColor(btn_active);
 	window.draw(btn);
 	window.draw(btn_text);
 	// grid 1,2
 	btn_text.setString("Hard");
-	btn_text.setPosition(grid_pos + sf::Vector2f(grid_spacing.x * (1 + 2) + btn_size.x * 2, grid_spacing.y * (2 + 1) + btn_size.y * 1 + char_size));
-	btn.setPosition(grid_pos + sf::Vector2f(grid_spacing.x * (1 + 2) + btn_size.x * 2, grid_spacing.y * (2 + 1) + btn_size.y * 1 + char_size));
+	btn_text.setPosition(row_1_btn_2_pos);
+	btn.setPosition(row_1_btn_2_pos);
 	btn.setFillColor(btn_inactive);
-	if (row_2_active_button == 2) btn.setFillColor(btn_active);
+	if (row_1_active_button == 2) btn.setFillColor(btn_active);
 	window.draw(btn);
 	window.draw(btn_text);
 
@@ -141,7 +140,7 @@ std::vector<int> Intro::display()
 	// activate antialiasing and create window
 	sf::ContextSettings settings;
 	settings.antialiasingLevel = 8;
-	sf::RenderWindow window(sf::VideoMode(400, 400), "Introduction", sf::Style::Default, settings);
+	sf::RenderWindow window(sf::VideoMode(400, 400), "Introduction", sf::Style::Close | sf::Style::Titlebar, settings);
 	while (window.isOpen())
 	{
 		sf::Event event;
@@ -163,7 +162,7 @@ std::vector<int> Intro::display()
 	}
 
 	// if active button values are unexpected default values are assumed
-	switch (row_1_active_button)
+	switch (row_0_active_button)
 	{
 	case 0: params[0] = params[1] = small_size;
 		break;
@@ -179,7 +178,7 @@ std::vector<int> Intro::display()
 	int easy_diff = params[0] * params[1] * 0.10;
 	int medium_diff = params[0] * params[1] * 0.15;
 	int hard_diff = params[0] * params[1] * 0.20;
-	switch (row_2_active_button)
+	switch (row_1_active_button)
 	{
 	case 0: params[2] = easy_diff;
 		break;
@@ -198,4 +197,5 @@ std::vector<int> Intro::display()
 void Intro::handle_mouse(sf::Vector2f m_pos)
 {
 	// mouse position logic
+
 }
--- Programowanie_obiektowe/Intro.h
+++ Programowanie_obiektowe/Intro.h
@@ -25,13 +25,22 @@ class Intro
 	const sf::Vector2f grid_pos = sf::Vector2f(15, 100);
 	const sf::Vector2f grid_spacing = sf::Vector2f(10, 10);
 	const sf::Vector2f btn_size = sf::Vector2f(60, 30);
-
+	const int title_char_size = 20;
+	// button and title positions
+	sf::Vector2f title_1_pos = grid_pos;
+	sf::Vector2f row_0_btn_0_pos = title_1_pos + sf::Vector2f(0, title_char_size + grid_spacing.y);
+	sf::Vector2f row_0_btn_1_pos = row_0_btn_0_pos + sf::Vector2f(btn_size.x + grid_spacing.x, 0);
+	sf::Vector2f row_0_btn_2_pos = row_0_btn_1_pos + sf::Vector2f(btn_size.x + grid_spacing.x, 0);
+	sf::Vector2f title_2_pos = row_0_btn_0_pos + sf::Vector2f(0, grid_spacing.y + btn_size.y);
+	sf::Vector2f row_1_btn_0_pos = title_2_pos + sf::Vector2f(0, title_char_size + grid_spacing.y);
+	sf::Vector2f row_1_btn_1_pos = row_1_btn_0_pos + sf::Vector2f(btn_size.x + grid_spacing.x, 0);
+	sf::Vector2f row_1_btn_2_pos = row_1_btn_1_pos + sf::Vector2f(btn_size.x + grid_spacing.x, 0);
 	// IMPORTANT - active buttons
 	// buttons are numbered from 0 to 2 and one in each row is active
 	// buttons are colored according to those values
 	// minesweeper values are changed accordingly to active button
+	int row_0_active_button = 0;
 	int row_1_active_button = 0;
-	int row_2_active_button = 0;
 
 public:
 	Intro();
